dff948dcb8a34360d22022e761a414d73d03c74c
Jake Prentice <prenticejake@me.com>
2021-11-15 17:22:05 +0000

    solved drawing issue

--- src/components/DrawCanvas/index.tsx
+++ src/components/DrawCanvas/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { ImgHTMLAttributes, useEffect } from 'react'
 import {StyledCanvas} from "./style";
 import useDrawingCanvas from "./useDrawingCanvas"
 
@@ -7,17 +7,22 @@ const DrawCanvas = ({predict}: {predict: (digit: string[] | number[]) => void}) 
     const {drawCanvas, ctx} = useDrawingCanvas({initialHeight: 300, initialWidth: 300});
     
     useEffect(() => {
-        drawCanvas.initStyles()
+        drawCanvas.initStyles({lineWidth: 26})
     }, [])
 
     const onClick = () => {
-        ctx?.drawImage(drawCanvas.ref.current!, 0, 0 ,28, 28)
-        const img = ctx?.getImageData(0,0, 28, 28)!;
+        const c = document.createElement("canvas");
+        const ctx2 = c.getContext("2d");
+        
+        //down scale image to 28x28
+        ctx2?.drawImage(drawCanvas.ref.current!, 0, 0 ,28, 28)
+
+        const img = ctx2?.getImageData(0,0, 28, 28)!;
+        
         const inputs = [1]
 
-
-        for (let i = 1; i <= 784; i++) {
-            inputs.push(img?.data[(i * 4) - 1]) 
+        for (let i = 0; i < img.data.length; i+= 4) {
+            inputs.push(img?.data[i + 3]); 
         }
 
        predict(inputs)
@@ -36,7 +41,10 @@ const DrawCanvas = ({predict}: {predict: (digit: string[] | number[]) => void}) 
             onMouseMove={drawCanvas.draw}
             onTouchMove={drawCanvas.draw}
         />
-        <button onClick={onClick}>click</button>
+        <div style={{marginBottom: "40px"}}>
+            <button onClick={onClick}>predict image</button>
+            <button onClick={drawCanvas.clear}>clear</button>
+        </div>
         </>
     )   
 }
